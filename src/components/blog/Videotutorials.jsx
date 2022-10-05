802d583a26736d545b038d49d5694c360324665a
MrGansoStr <ccounttnro1@gmail.com>
2022-10-04 20:29:37 -0500

    tercer commit

--- src/components/blog/Videotutorials.jsx
+++ src/components/blog/Videotutorials.jsx
@@ -3,9 +3,6 @@ export const Videotutorials = () => {
   return (
     <>
     <div className="container-xl">
-    <ReactPlayer url={require('./Videos/Duro 2 horas, acto acto pide contacto -  Faraón Love Shady  [ Vídeo Oficial ].mp4')} width="100%" height="300px" controls/>
-    </div>
-    <div className="container-xl">
       <ReactPlayer url={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}
       width="100%" height="300px" controls/>
     </div>
